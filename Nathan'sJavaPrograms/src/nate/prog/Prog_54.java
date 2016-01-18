becb9fa7d6b4f9b284566a7946a1743211db9f09
gandalfthewizard <gandalfthewizard2207@gmail.com>
2016-01-18 15:00:22 -0500

    Started the program, though moved on to another.

--- Nathan'sJavaPrograms/src/nate/prog/Prog_54.java
+++ Nathan'sJavaPrograms/src/nate/prog/Prog_54.java
@@ -3,7 +3,19 @@ package nate.prog;
 public class Prog_54 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		//speed of the cars in mph
+		
+		
+		//Perameters of the problem
+		double diameter = 8000;
+		double radius = diameter / 2;
+		
+		
+		
+		System.out.println("The Blue Car will corss the finish line"  "seconds after the red car");
+		
+		
 
 	}
 
