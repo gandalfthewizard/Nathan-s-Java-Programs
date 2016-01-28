cb9fd8523b02c312792f6cac2dd93902a782fd74
gandalfthewizard <gandalfthewizard2207@gmail.com>
2016-01-28 18:04:07 -0500

    Finished Program 122h, learned some new stuff. First use of for loops.

--- Nathan'sJavaPrograms/src/nate/prog/Prog_122h.java
+++ Nathan'sJavaPrograms/src/nate/prog/Prog_122h.java
@@ -1,18 +1,16 @@
 package nate.prog;
 
 public class Prog_122h {
-	public static void main(String[] args ) {
-		
-		System.out.println("Number    Square      Square root    Cube   4th Root");
-for (int num = 1; num < 20; num++) {		
-	
-	System.out.println(String.format("%2d", num) +"        " +String.format("%4d", num*num) +"        "+String.format("%4f", Math.sqrt(num)) +"        " +String.format("%4d", (num*num*num))
-	+"        " + String.format("%4f", Math.pow(num, 1/4)));
-}
-		
-		
+	public static void main(String[] args) {
+
+		System.out.println("Number    Square      Square root      Cube       4th Root");
+		for (int num = 1; num < 20; num++) {
+
+			System.out.println(String.format("%2d", num) + "        " + String.format("%4d", num * num) + "        "
+					+ String.format("%4f", Math.sqrt(num)) + "        " + String.format("%4d", (num * num * num))
+					+ "        " + String.format("%4f", Math.pow(num, 1.0 / 4.0)));
+		}
+
 	}
-	
+
 }
-
-
